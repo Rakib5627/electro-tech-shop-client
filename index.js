07b3525131638adb3797231221352cf2a3c7b0d6
Rakibul Islam <mdrakib.5627@gmail.com>
2023-10-20 20:33:39 +0600

    get fixed

--- index.js
+++ index.js
@@ -142,9 +142,9 @@ run().catch(console.dir);
 
 
 
-// app.get('/' , (req , res) =>{
-//     res.send('abcd running');
-// })
+app.get('/' , (req , res) =>{
+    res.send('server running');
+})
 
 app.listen(port , () => {
     console.log(`server running on port : ${port}`);
